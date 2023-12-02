0c1b2c9968b2c49be3f580044779fb024118c26e
hindunge <119276230+hindunge@users.noreply.github.com>
2023-12-02 18:23:44 +0900

    JS-로직구현하기

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ const header = document.querySelector('.header');
 const headerHeight = header.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
   //스크롤되는 Y 좌표가 headerHeight보다 크다면 다른 스타일링!
-  console.log(window.scrollY);
+
   if (window.scrollY > headerHeight) {
     header.classList.add('header--dark');
   } else {
