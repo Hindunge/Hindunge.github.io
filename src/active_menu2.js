477064d9a01252a39d2b93c78b6d457dfb7aa629
hindunge <119276230+hindunge@users.noreply.github.com>
2023-12-03 02:23:54 +0900

    JS-로직구현하기2

--- src/active_menu2.js
+++ src/active_menu2.js
@@ -1,10 +1,10 @@
 // 구현 계획
 // 1. 모든 섹션 요소들과 메뉴 아이템들을 가지고 온다.
-//2. IntersectionObserver를 사용해서 모든 섹션들을 관찰해야 한다.
-//3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다.
+// 2. IntersectionObserver를 사용해서 모든 섹션들을 관찰해야 한다.
+// 3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다.
 // 보여지는 섹션:
-// -다수의 섹션이 동시에 보여진다면, 가장 첫번째 섹션을 선택
-// -마지막 contact 섹션이 보여진다면, 그러면 가장 마지막 섹션을 선택
+// - 다수의 섹션이 동시에 보여진다면, 가장 첫번째 섹션을 선택
+// - 마지막 contact 섹션이 보여진다면, 그러면 가장 마지막 섹션을 선택
 const sectionIds = [
   '#home',
   '#about',
@@ -19,15 +19,12 @@ const navItems = sectionIds.map((id) =>
 );
 const visibleSections = sectionIds.map(() => false);
 
-const options = {
-  rootmargin: '-20% 0px 0px 0px',
-  threshold: [0, 1],
-};
+const options = {};
 const observer = new IntersectionObserver(observerCallback, options);
 sections.forEach((section) => observer.observe(section));
 
 function observerCallback(entries) {
-  let selectLastOne; //flag 변수
+  let selectLastOne;
   entries.forEach((entry) => {
     const index = sectionIds.indexOf(`#${entry.target.id}`);
     visibleSections[index] = entry.isIntersecting;
